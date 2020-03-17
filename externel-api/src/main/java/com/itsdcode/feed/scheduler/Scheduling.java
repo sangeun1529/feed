641ffe228246029b8b771c85bb684a03dbc5cab5
sangeun1529 <32221320+sangeun1529@users.noreply.github.com>
2020-03-17 21:39:48 +0900

    스케쥴러
    스케쥴러 dummy 데이터 시간차 curd 개발

---
+++ externel-api/src/main/java/com/itsdcode/feed/scheduler/Scheduling.java
@@ -0,0 +1,7 @@
+package com.itsdcode.feed.scheduler;
+
+public interface Scheduling {
+
+    public boolean cron();
+    public void excute();
+}
---
+++ externel-api/src/main/java/com/itsdcode/feed/scheduler/quartz/QuartzDetail.java
@@ -0,0 +1,4 @@
+package com.itsdcode.feed.scheduler.quartz;
+
+public class QuartzDetail {
+}
